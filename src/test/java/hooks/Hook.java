727c88a7da538a9a85d3ef07c80d7abcac98f143
umitgulsun <umit.gulsun.mail@gmail.com>
2023-05-01 00:34:30 +0300

    first commijt

---
+++ src/test/java/hooks/Hook.java
@@ -0,0 +1,27 @@
+package hooks;
+
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+
+
+public class Hook
+{
+
+
+    @After
+    public void tearDownScenarios(Scenario scenario){
+//        Eger bir Scenario FAIL ederse, ekran goruntusunu al ve rapora ekle
+        if (scenario.isFailed()) {
+            final byte[] failedScreenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//                       ekran goruntusu    file tipi                  ekran goruntusunun adi
+            scenario.attach(failedScreenshot, "image/png", "failed_scenario_" + scenario.getName());
+        }
+    }
+
+}
---
+++ src/test/java/utilities/MyRemoteDriver.java
@@ -0,0 +1,46 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class MyRemoteDriver
+{
+    private static WebDriver crossDriver;
+    public static WebDriver getRemoteDriverSetup(String url, AbstractDriverOptions options)
+    {
+        try
+        {
+            crossDriver = new RemoteWebDriver(new URL(url), options);
+        }
+        catch(MalformedURLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        crossDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+        crossDriver.manage().window().maximize();
+        return crossDriver;
+    }
+    public static WebDriver getRemoteDriver()
+    {
+        return crossDriver;
+    }
+    public static AbstractDriverOptions getOptions(String browserOptions)
+    {
+        AbstractDriverOptions options = null;
+        if(browserOptions.equals("chrome"))
+            options = new ChromeOptions();
+        else if(browserOptions.equals("firefox"))
+            options = new FirefoxOptions();
+        else if(browserOptions.equals("edge"))
+            options = new EdgeOptions();
+        return options;
+    }
+}
